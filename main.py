994c82a2bbaa0bafac7e9d0ce462f6c944819106
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-08-07 00:42:36 +0530

    Changes

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI() # This is an instance of the FastAPI Class
 
 # Get is an operation in API
-@app.get("/")
-def index():
-    return {"data" : 'blog list'}
+@app.get("/blog")
+def index(limit: int=10, published: bool=True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {"data" : f'{limit} published blog list from db'}
+    else:
+        return {"data" : f"{limit} blog list from db"}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data" : "All Unpublished blogs are here"}
 
 @app.get("/blog/{id}")
-def show(id):
+def show(id: int): # you can use data type as you need like int, float, string
     return {"data" : id}
 
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data" : {'1','2'}}
