f6b9aa6615b77bcf023adfbebaccb2a7517178cc
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-08-07 00:08:30 +0530

    Changes are made

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # This is an instance of the FastAPI Class
 
+# Get is an operation in API
 @app.get("/")
 def index():
-    return 'This is Saidaiwik'
+    return {"data" : 'blog list'}
 
-@app.get("/about")
-def about():
-    return "This page will be about me"
+@app.get("/blog/{id}")
+def show(id):
+    return {"data" : id}
+
