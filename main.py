f1cb3a807c2d05c178d98042161ceea86ccfe69e
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-08-08 11:23:09 +0530

    Commited

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from typing import Optional
+from typing import Optional # this is for query parameter purpose
+from pydantic import BaseModel # this is for POST operation in API
+import uvicorn
 
 app = FastAPI() # This is an instance of the FastAPI Class
 
@@ -24,3 +26,17 @@ def show(id: int): # you can use data type as you need like int, float, string
 @app.get("/blog/{id}/comments")
 def comments(id):
     return {"data" : {'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {'data' : f"Blog is created with title called {blog.title}"}
+
+# For debugging purpose uncomment these lines when you are debugging
+# if __name__ == "__main__":
+#     uvicorn.run(app,host="127.0.0.1",port=9000)
