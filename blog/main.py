334a0a402f8e59c7b89788b44d961efb6b277859
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-08-08 12:10:20 +0530

    Changes Done

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import scehemas
 
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str
-
 @app.post("/blog")
-def create(request: Blog):
+def create(request: scehemas.Blog):
     # return {'title': request.title,'body' : request.body}
     return request
