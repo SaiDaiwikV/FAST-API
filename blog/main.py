335b1bdd32887b2de770dc0415f82ba31bfc340d
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-08-08 12:38:39 +0530

    DataBase is created

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from . import scehemas
+from . import models
+from .database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
 
 @app.post("/blog")
 def create(request: scehemas.Blog):
